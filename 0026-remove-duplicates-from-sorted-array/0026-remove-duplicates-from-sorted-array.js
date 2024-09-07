53a50233596e407415c9e4cb655e32ca89d698a4
DurgaPrasad K <142313714+korukondadurgaprasad@users.noreply.github.com>
2024-09-07 16:58:40 +0530

    Time: 68 ms (40.56%), Space: 51.6 MB (92.23%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let k=0;
+    for (var i=0;i<nums.length;i++){
+        if (nums[i]!==nums[i+1]){
+            nums[k]=nums[i];
+            k++;
+        }
+    }
+        return k;
+
+
+    
+};
