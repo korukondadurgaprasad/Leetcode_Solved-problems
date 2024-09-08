1404e6d9cae28ffbd712a6f17ea771a9d0f75923
DurgaPrasad K <142313714+korukondadurgaprasad@users.noreply.github.com>
2024-09-08 20:40:57 +0530

    Time: 67 ms (88.78%), Space: 53.7 MB (78.58%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let j=0;
+    for (var i=0;i<nums.length;i++)
+    {
+        if (nums[i]!==0){
+            nums[j]=nums[i];
+            j++
+        }
+    }
+
+    while (j<nums.length)
+    {
+        nums[j]=0;
+        j++;
+    }
+    
+};
