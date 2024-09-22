1d7a7bc7aefc5be8b55f69f478a7e4f0c6482ddc
DurgaPrasad K <142313714+korukondadurgaprasad@users.noreply.github.com>
2024-09-22 18:30:25 +0530

    Time: 71 ms (57.14%), Space: 53 MB (12.58%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.js
+++ 0263-ugly-number/0263-ugly-number.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 var isUgly = function(n) {
-     if (n <= 0) return false;
+    if (n <= 0) return false;
     let cons=[2,3,5]
     for(let i of cons){
         while(n%i===0){
