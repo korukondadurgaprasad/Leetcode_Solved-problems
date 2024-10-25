e3873ac2aac29902894c983d08d0104a4e065e32
DurgaPrasad K <142313714+korukondadurgaprasad@users.noreply.github.com>
2024-10-25 15:15:24 +0530

    Time: 3 ms (99.44%), Space: 11.6 MB (74.21%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def romanToInt(self, s):
+        rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 
+               'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        length = len(s)
+        
+        for i in range(length):
+            # Check if the current numeral is less than the next numeral
+            if i <length-1 and rom[s[i]] < rom[s[i + 1]]:
+                total -= rom[s[i]]
+            else:
+                total += rom[s[i]]
+                
+        return total
