1b5e600d1d955819b4752e19708015ebda88d94b
DurgaPrasad K <142313714+korukondadurgaprasad@users.noreply.github.com>
2024-10-25 14:29:55 +0530

    Time: 18 ms (41.28%), Space: 11.5 MB (88.91%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def reverse(self, x):
+      min = (-2**31)
+      max=(2**31)-1
+      if x<0:
+        sign=-1
+      else:
+        sign =1
+      x=abs(x)
+      res =int(str(x)[::-1])*sign
+      
+      if res<min or res>max:
+        return 0
+      else:
+        return res
