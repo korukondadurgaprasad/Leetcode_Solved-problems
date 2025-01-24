d769b61d898bd662c14f61a5ac61dcf1005f0916
DurgaPrasad K <142313714+korukondadurgaprasad@users.noreply.github.com>
2025-01-24 22:23:59 +0530

    Time: 0 ms (100%), Space: 13.2 MB (47.23%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,17 @@
 class Solution(object):
     def twoSum(self, nums, target):
+
+        # ---O(N2)--
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i]+nums[j]==target:
+        #             return i,j     
+        # ---O(N)--
+        num_index_map = {}
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    return i,j       
+            complement = target - nums[i]
+            if complement in num_index_map:
+                return [num_index_map[complement], i]
+
+            num_index_map[nums[i]] = i
+        return []
