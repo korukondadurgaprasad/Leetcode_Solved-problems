24ff2326ef821433aec1d0d339e6eca5b3d4186b
DurgaPrasad K <142313714+korukondadurgaprasad@users.noreply.github.com>
2025-02-13 22:18:30 +0530

    Time: 13 ms (38.97%), Space: 14.5 MB (10.57%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -12,8 +12,13 @@ class Solution(object):
             else:
                 count -= 1
         
+        
         # Phase 2: Verify the candidate (Optional since the problem guarantees a majority element)
-        if nums.count(candidate) > len(nums) // 2:
+        c=0
+        for i in range(len(nums)):
+            if nums[i]==candidate:
+                c+=1
+        if c > len(nums) // 2:
             return candidate
         return None  # This line will not be reached if a majority element is guaranteed
 
