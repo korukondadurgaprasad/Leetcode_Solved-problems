2c15b4621b2b0c354af4559b00b0a09f9d5a3b32
DurgaPrasad K <142313714+korukondadurgaprasad@users.noreply.github.com>
2025-06-17 21:55:15 +0530

    Time: 0 ms (100%), Space: 12.5 MB (58.09%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,4 +1,9 @@
 class Solution(object):
+    def generate(self, numRows):
+        result = []
+        for i in range(1, numRows + 1):
+            result.append(self.generateRow(i))
+        return result
     def generateRow(self, row):
         ans = 1
         ansRow = [1]  # The first element is always 1
@@ -9,8 +14,3 @@ class Solution(object):
         
         return ansRow
 
-    def generate(self, numRows):
-        result = []
-        for i in range(1, numRows + 1):
-            result.append(self.generateRow(i))
-        return result
