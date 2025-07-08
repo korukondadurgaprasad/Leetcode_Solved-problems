097057de57a5e508132781d7674b9337316dfaa5
DurgaPrasad K <142313714+korukondadurgaprasad@users.noreply.github.com>
2025-07-08 10:15:15 +0530

    Time: 7 ms (88.13%), Space: 13.6 MB (59.37%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,17 +1,19 @@
 class Solution(object):
     def isIsomorphic(self, s, t):
-        if len(s) != len(t):
+        if len(s)!=len(t):
             return False
         st={}
         ts={}
         for i in range(len(s)):
-            c1,c2=s[i],t[i]
+            c1=s[i]
+            c2=t[i]
             if c1 in st:
                 if st[c1]!=c2:
                     return False
             elif c2 in ts:
                 if ts[c2]!=c1:
                     return False
+
             else:
                 st[c1]=c2
                 ts[c2]=c1
