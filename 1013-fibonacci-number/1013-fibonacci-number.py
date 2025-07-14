9457282a44ab31dc05cd653d9cb715c0306bb6b4
DurgaPrasad K <142313714+korukondadurgaprasad@users.noreply.github.com>
2025-07-14 17:03:47 +0530

    Time: 658 ms (21.57%), Space: 12.5 MB (49.99%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -4,12 +4,14 @@ class Solution(object):
             return 0
         elif n == 1:
             return 1
-        a = 0
-        b = 1
-        for i in range(2, n + 1):  # Loop n - 1 times, starting from 2 up to n
-            c = a + b
-            a = b
-            b = c
-        return b
+        return self.fib(n - 1) + self.fib(n - 2)
+
+        # a = 0
+        # b = 1
+        # for i in range(2, n + 1):  # Loop n - 1 times, starting from 2 up to n
+        #     c = a + b
+        #     a = b
+        #     b = c
+        # return b
 
         
