4d511437f0927223f369ccd743c17196794c1441
DurgaPrasad K <142313714+korukondadurgaprasad@users.noreply.github.com>
2025-07-22 16:56:07 +0530

    Time: 3 ms (58.27%), Space: 12.5 MB (48.99%) - LeetHub

--- 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.py
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.py
@@ -1,11 +1,34 @@
 class Solution(object):
     def countGoodSubstrings(self, s):
-        st=set()
-        count=0
-        for i in range(len(s)):
-            for j in range(i,len(s)):
-                res=s[i:j+1]
-                if len(res)==3 and len(set(res))==3:
-                    count+=1
-        return count
+        l=0
+        dic={}
+        cnt=0
+        k=3
+        for r in range(len(s)):
+            if s[r] in dic:
+                dic[s[r]]+=1
+            else:
+                dic[s[r]]=1
+            if r-l==k:
+                dic[s[l]]-=1
+                if dic[s[l]]==0:
+                    dic.pop(s[l])
+                l+=1
+            if len(dic)==k:
+                cnt+=1
+        return cnt
+
+
+
+
+
+
+        # st=set()
+        # count=0
+        # for i in range(len(s)):
+        #     for j in range(i,len(s)):
+        #         res=s[i:j+1]
+        #         if len(res)==3 and len(set(res))==3:
+        #             count+=1
+        # return count
         
